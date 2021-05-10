2579fd22f9340fd88465815c29f1eb7ecf6c3c22
Patrizio Germani <patriziogermani@MacBook-Air-di-Patrizio.local>
2021-05-10 17:47:41 +0200

    modifica patrizio

--- Gestore_Ristorante/src/Prova.java
+++ Gestore_Ristorante/src/Prova.java
@@ -3,5 +3,7 @@ public class Prova {
 	public static void main(String[] args){
 		System.out.println("ciaooooooooooooo");
 		System.out.println("Prova Dan");
+		
+		System.out.println("prova pat");
 	}
 }
